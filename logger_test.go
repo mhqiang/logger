d7037e9f04ae4382a9ec56ca68a820c59d5ee7ea
马华强 <mahuaqiang@mahuaqiangdeMacBook-Pro.local>
2022-02-09 15:30:12 +0800

    first commit

---
+++ logger_test.go
@@ -0,0 +1,18 @@
+package log
+
+import (
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+
+	var config Config
+
+	config.MaxLogSize = 100
+	config.ServiceName = "test"
+	config.Level = "warn"
+	config.NotDisplayLine = true
+	Init(&config)
+	Info("aaa")
+	Error("bbb")
+}
